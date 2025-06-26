d317fa61f6b4b6eee561d5d035f93e594bd01fd8
azya11 <ashamura@asu.edu>
2025-06-26 14:20:44 +0500

    [+] Split the Commands file to 2 files, for architechture simplisity

--- commands/Command.cs
+++ commands/Command.cs
@@ -10,6 +10,7 @@ using DSharpPlus.CommandsNext.Attributes;
 using DSharpPlus.Entities;
 using DSharpPlus.EventArgs;
 using DSharpPlus.Interactivity.Extensions;
+using MyTaskManagerBot.commands;
 
 namespace MyTaskManagerBot.commands
 {
@@ -132,7 +133,7 @@ namespace MyTaskManagerBot.commands
                         {
                             game.IsReady = true; //Set the game to ready
                             await ctx.Channel.SendMessageAsync("WE ARE STARTING!!! \nSORRY FOR EVERYONE WHO WAS LATE!"); //Send a message that the game is ready
-                            game.StartGame(); //Start the game
+                            await game.StartGame(); //Start the game
                         }
                         else
                         {
@@ -186,96 +187,96 @@ namespace MyTaskManagerBot.commands
         }
     }
 
-        //CLASS GAME
-        public class Game
-    {
-        //events
-
-        // Parameters for the game
-        public bool IsReady { get; set; }
-        public ulong ChannelId { get; set; } //Channel ID where the game is played                                   
-        public List<Player> Players { get; set; } //List of players in the game
-        public int mafia { get; set; }
-        public int doctor { get; set; }
-        public int cop { get; set; }
-
-
-        // Constructor to initialize a new game with the channel ID
-        public Game(ulong channelId)
-        {
-            ChannelId = channelId;
-            Players = new List<Player>();
-            IsReady = false;
-        }
-
-        public async Task addMafia()
-        {
-            this.mafia++;
-        }
-
-        public async Task addDoc()
-        {
-            this.doctor++;
-        }
-        public async Task addCop()
-        {
-            this.cop++;
-        }
-
-        public async Task AddPlayer(DiscordMember playerId)
-        {
-            Player player = new Player(playerId);
-            if (!Players.Contains(player)) //Check if the player is already in the game
-            {
-                Players.Add(player);
-            }
-
-        }
-
-        public async Task StartGame()
-        {
-            bool live = true;
-            //Assign the roles to the players, without any rules yet.
-            while (live == true)
-            {
-                foreach (Player player in Players)
-                {
-                    //while (this.Players.= true)
-                    //{
-                    //    async sendMessage(Player)
-                    //}
-                }
-                
-                //check the people that are alive
-                // send them a turn notifications
-                // wait for input
-                // give the output
-            }
-            //Send each player their role assignment
-            // This could include assigning roles, sending messages to players, etc.
-        }
-
-    }
-
-    //public async Task sendMessage(Player targetPlayer)
+    //    //CLASS GAME
+    //    public class Game
     //{
-    //    //logic to send message to the players if there are alive
-    //}
-    public class Player
-    {
-        // 0 - Civillian
-        // 1 - Mafia
-        // 2 - Doctor
-        // 3 - Cop
-        public int role { get; set; }
-        public DiscordMember Member { get; set; }
-        public bool isLive { get; set; }
+    //    //events
 
-        public Player(DiscordMember Member)
-        {
-            this.Member = Member;
-            this.role = 0;
-            this.isLive = true;
-        }
-    }
+    //    // Parameters for the game
+    //    public bool IsReady { get; set; }
+    //    public ulong ChannelId { get; set; } //Channel ID where the game is played                                   
+    //    public List<Player> Players { get; set; } //List of players in the game
+    //    public int mafia { get; set; }
+    //    public int doctor { get; set; }
+    //    public int cop { get; set; }
+
+
+    //    // Constructor to initialize a new game with the channel ID
+    //    public Game(ulong channelId)
+    //    {
+    //        ChannelId = channelId;
+    //        Players = new List<Player>();
+    //        IsReady = false;
+    //    }
+
+    //    public async Task addMafia()
+    //    {
+    //        this.mafia++;
+    //    }
+
+    //    public async Task addDoc()
+    //    {
+    //        this.doctor++;
+    //    }
+    //    public async Task addCop()
+    //    {
+    //        this.cop++;
+    //    }
+
+    //    public async Task AddPlayer(DiscordMember playerId)
+    //    {
+    //        Player player = new Player(playerId);
+    //        if (!Players.Contains(player)) //Check if the player is already in the game
+    //        {
+    //            Players.Add(player);
+    //        }
+
+    //    }
+
+    //    public async Task StartGame()
+    //    {
+    //        bool live = true;
+    //        //Assign the roles to the players, without any rules yet.
+    //        while (live == true)
+    //        {
+    //            foreach (Player player in Players)
+    //            {
+    //                //while (this.Players.= true)
+    //                //{
+    //                //    async sendMessage(Player)
+    //                //}
+    //            }
+                
+    //            //check the people that are alive
+    //            // send them a turn notifications
+    //            // wait for input
+    //            // give the output
+    //        }
+    //        //Send each player their role assignment
+    //        // This could include assigning roles, sending messages to players, etc.
+    //    }
+
+    //}
+
+    ////public async Task sendMessage(Player targetPlayer)
+    ////{
+    ////    //logic to send message to the players if there are alive
+    ////}
+    //public class Player
+    //{
+    //    // 0 - Civillian
+    //    // 1 - Mafia
+    //    // 2 - Doctor
+    //    // 3 - Cop
+    //    public int role { get; set; }
+    //    public DiscordMember Member { get; set; }
+    //    public bool isLive { get; set; }
+
+    //    public Player(DiscordMember Member)
+    //    {
+    //        this.Member = Member;
+    //        this.role = 0;
+    //        this.isLive = true;
+    //    }
+    //}
 }
---
+++ commands/MafiaLogic.cs
@@ -0,0 +1,91 @@
+﻿using DSharpPlus.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using MyTaskManagerBot.commands;
+
+public class Game
+{
+    //events
+
+    // Parameters for the game
+    public bool IsReady { get; set; }
+    public ulong ChannelId { get; set; } //Channel ID where the game is played                                   
+    public List<Player> Players { get; set; } //List of players in the game
+    public int mafia { get; set; }
+    public int doctor { get; set; }
+    public int cop { get; set; }
+
+
+    // Constructor to initialize a new game with the channel ID
+    public Game(ulong channelId)
+    {
+        ChannelId = channelId;
+        Players = new List<Player>();
+        IsReady = false;
+    }
+
+    public async Task addMafia()
+    {
+        this.mafia++;
+    }
+
+    public async Task addDoc()
+    {
+        this.doctor++;
+    }
+    public async Task addCop()
+    {
+        this.cop++;
+    }
+
+    public async Task AddPlayer(DiscordMember playerId)
+    {
+        Player player = new Player(playerId);
+        if (!Players.Contains(player)) //Check if the player is already in the game
+        {
+            Players.Add(player);
+        }
+
+    }
+
+    public async Task StartGame()
+    {
+        bool live = true;
+        //Assign the roles to the players, without any rules yet.
+        while (live == true)
+        {
+            foreach (Player player in Players)
+            {
+                //while (this.Players.= true)
+                //{
+                //    async sendMessage(Player)
+                //}
+            }
+
+            //check the people that are alive
+            // send them a turn notifications
+            // wait for input
+            // give the output
+        }
+        //Send each player their role assignment
+        // This could include assigning roles, sending messages to players, etc.
+    }
+
+}
+public class Player
+{
+    // 0 - Civillian
+    // 1 - Mafia
+    // 2 - Doctor
+    // 3 - Cop
+    public int role { get; set; }
+    public DiscordMember Member { get; set; }
+    public bool isLive { get; set; }
+
+    public Player(DiscordMember Member)
+    {
+        this.Member = Member;
+        this.role = 0;
+        this.isLive = true;
+    }
+}
